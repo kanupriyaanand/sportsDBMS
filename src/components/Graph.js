a04d0d0cb23f5d15b9874f1eae56a705181e9a15
viku3150 <21vikashgupta@gmail.com>
2023-02-05 14:31:47 +0530

    update

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -3,18 +3,22 @@ import Chart from "react-apexcharts";
 
 const Graph = ({ data }) => {
   console.log(data);
+  const game = [
+    "football",
+    "cricket",
+    "yoga",
+    "hockey",
+    "basketball",
+    "gymnastics",
+    "badminton",
+  ];
 
   const options = {
-    labels: [
-      "football",
-      "cricket",
-      "yoga",
-      "hockey",
-      "basketball",
-      "gymnastics",
-      "badminton",
-    ],
+    xaxis: {
+      categories: game, //will be displayed on the x-asis
+    },
   };
+
   return (
     <div className="bg-white w-fit mt-5 mx-auto rounded-2xl p-5">
       <Chart options={options} type="pie" series={data} width="380" />
