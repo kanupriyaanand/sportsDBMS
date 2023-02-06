35bd284bbace9b2ff412ccf84b53d9c30ad40fdd
viku3150 <21vikashgupta@gmail.com>
2023-02-06 22:17:13 +0530

    done

--- src/Screens/SignInScreen.js
+++ src/Screens/SignInScreen.js
@@ -4,11 +4,7 @@ import db, { auth } from "../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import toast, { Toaster } from "react-hot-toast";
 import { addDoc, collection } from "firebase/firestore";
-import { ErrorMessage } from '@hookform/error-message';
-
-
-
-
+import { ErrorMessage } from "@hookform/error-message";
 
 const SignInScreen = ({ setSignIn }) => {
   const {
@@ -17,10 +13,24 @@ const SignInScreen = ({ setSignIn }) => {
     reset,
     formState: { errors },
   } = useForm();
-  
-const departmentNames = [
-  "AS","IS","CS","EC","ET","ME","CV","IM", "M.Tech", "MCA", "BT","CH", "EI", "EE", "AIML"
-]
+
+  const departmentNames = [
+    "AS",
+    "IS",
+    "CS",
+    "EC",
+    "ET",
+    "ME",
+    "CV",
+    "IM",
+    "M.Tech",
+    "MCA",
+    "BT",
+    "CH",
+    "EI",
+    "EE",
+    "AIML",
+  ];
   const onSubmit = async (data) => {
     console.log(data);
     await createUserWithEmailAndPassword(auth, data.Email, data.password)
@@ -43,8 +53,6 @@ const departmentNames = [
   console.log(errors);
 
   return (
-
-    
     <div className="bg-black bg-opacity-60 rounded-lg p-5 my-auto">
       <Toaster position="top-center" reverseOrder={false} />
       <h1 className="text-center mb-4 text-white text-2xl">Sign Up</h1>
@@ -68,33 +76,39 @@ const departmentNames = [
           type="text"
           className="px-3 py-2 rounded-md"
           placeholder="Email"
-          {...register("Email", { required: true, pattern:{ value:/^\S+@rvce.edu.in+$/i , message: "enter a valid RVCE email number"}})}
-        
-        /> 
+          {...register("Email", {
+            required: true,
+            pattern: {
+              value: /^\S+@rvce.edu.in+$/i,
+              message: "enter a valid RVCE email number",
+            },
+          })}
+        />
         <div className="text-red-700">{errors.Email?.message}</div>
         {/* <ErrorMessage errors={errors} name="please enter valid email ID" /> */}
-        <input 
+        <input
           type="tel"
           className="px-3 py-2 rounded-md"
           placeholder="Mobile number"
           {...register("Mobile_number", {
             required: true,
-            
+
             minLength: {
               value: 10,
-            message: "enter a 10 digit phone number",
-          
-          }, 
-          maxLength: {
-            value: 10,
-          message: "enter a 10 digit phone number" },
-        pattern:{
-          value:/^\S[0-9]*$/i,
-          message: "only numbers to be entered"
-        }})}
+              message: "enter a 10 digit phone number",
+            },
+            maxLength: {
+              value: 10,
+              message: "enter a 10 digit phone number",
+            },
+            pattern: {
+              value: /^\S[0-9]*$/i,
+              message: "only numbers to be entered",
+            },
+          })}
         />
-        <div className="text-red-700">{errors.Email?.message}</div>
-       <div className="text-red-700">{errors.Mobile_number?.message}</div>
+
+        <div className="text-red-700">{errors.Mobile_number?.message}</div>
         <input
           type="text"
           className="px-3 py-2 rounded-md"
@@ -122,24 +136,33 @@ const departmentNames = [
         <input
           className="px-3 py-2 rounded-md"
           type="number"
+          min={1}
+          max={8}
           placeholder="Semester"
-          {...register("semester", 'test', { required: true ,  pattern:{
-            value: 2||1||3||4||5||6||7||8
-            ,
-            message: "enter a number between 1 and 8"
-           },
-           maxLength: { value:1, message: "enter a number between 1 and 8"}
-        
-
-         })}
+          {...register("semester", {
+            required: true,
+            maxLength: { value: 1, message: "enter a number between 1 and 8" },
+            min: {
+              value: 1,
+              message: "Enter a value greater than or equal to 1",
+            },
+            max: {
+              value: 8,
+              message: "Enter a value less than or equal to 8",
+            },
+          })}
         />
-         <div className="text-red-700">{errors.semester?.message}</div>
-        
+        <div className="text-red-700">{errors.semester?.message}</div>
+
         <input
           className="px-3 py-2 rounded-md"
           type="date"
+          min="1990-01-01"
+          max="2006-12-31"
           placeholder="Date of Birth"
-          {...register("date_of_birth", { required: true , max:{value:`31-12-2006`} })}
+          {...register("date_of_birth", {
+            required: true,
+          })}
         />
         <input
           className="px-3 py-2 rounded-md"
@@ -151,14 +174,24 @@ const departmentNames = [
           className="px-3 py-2 rounded-md"
           type="text"
           placeholder="USN"
-          {...register("USN", { required: true, maxLength:{value:10, message:"enter valid USN"}})}
+          {...register("USN", {
+            required: true,
+            pattern: {
+              value: /(?:1RV)(?:\d)(?:\d)(?:[A-Z])(?:[A-Z])(?:\d)(?:\d)(?:\d)$/,
+              message: "Please check your USN",
+            },
+            maxLength: { value: 10, message: "enter valid USN" },
+          })}
         />
         <div className="text-red-700">{errors.USN?.message}</div>
         <input
           className="px-3 py-2 rounded-md"
           type="email"
           placeholder="Counselor email"
-          {...register("Counselor_email", { required: true, pattern: /^\S+@rvce.edu.in+$/i })}
+          {...register("Counselor_email", {
+            required: true,
+            pattern: /^\S+@rvce.edu.in+$/i,
+          })}
         />
 
         <button
