c33fe52193e58d734a424b64eddff705ec69c658
kanupriyaanand <kanupriya.a01@gmail.com>
2023-02-06 21:33:24 +0530

    regex

--- src/Screens/SignInScreen.js
+++ src/Screens/SignInScreen.js
@@ -6,6 +6,10 @@ import toast, { Toaster } from "react-hot-toast";
 import { addDoc, collection } from "firebase/firestore";
 import { ErrorMessage } from '@hookform/error-message';
 
+
+
+
+
 const SignInScreen = ({ setSignIn }) => {
   const {
     register,
@@ -13,6 +17,10 @@ const SignInScreen = ({ setSignIn }) => {
     reset,
     formState: { errors },
   } = useForm();
+  
+const departmentNames = [
+  "AS","IS","CS","EC","ET","ME","CV","IM", "M.Tech", "MCA", "BT","CH", "EI", "EE", "AIML"
+]
   const onSubmit = async (data) => {
     console.log(data);
     await createUserWithEmailAndPassword(auth, data.Email, data.password)
@@ -35,6 +43,8 @@ const SignInScreen = ({ setSignIn }) => {
   console.log(errors);
 
   return (
+
+    
     <div className="bg-black bg-opacity-60 rounded-lg p-5 my-auto">
       <Toaster position="top-center" reverseOrder={false} />
       <h1 className="text-center mb-4 text-white text-2xl">Sign Up</h1>
@@ -58,10 +68,11 @@ const SignInScreen = ({ setSignIn }) => {
           type="text"
           className="px-3 py-2 rounded-md"
           placeholder="Email"
-          {...register("Email", { required: true, pattern: /^\S+@\S+$/i })}
+          {...register("Email", { required: true, pattern:{ value:/^\S+@rvce.edu.in+$/i , message: "enter a valid RVCE email number"}})}
         
         /> 
-        <ErrorMessage errors={errors} name="please enter valid email ID" />
+        <div className="text-red-700">{errors.Email?.message}</div>
+        {/* <ErrorMessage errors={errors} name="please enter valid email ID" /> */}
         <input 
           type="tel"
           className="px-3 py-2 rounded-md"
@@ -71,9 +82,18 @@ const SignInScreen = ({ setSignIn }) => {
             
             minLength: {
               value: 10,
-            message: "enter a valid phone number"
-          }})}
+            message: "enter a 10 digit phone number",
+          
+          }, 
+          maxLength: {
+            value: 10,
+          message: "enter a 10 digit phone number" },
+        pattern:{
+          value:/^\S[0-9]*$/i,
+          message: "only numbers to be entered"
+        }})}
         />
+        <div className="text-red-700">{errors.Email?.message}</div>
        <div className="text-red-700">{errors.Mobile_number?.message}</div>
         <input
           type="text"
@@ -101,9 +121,17 @@ const SignInScreen = ({ setSignIn }) => {
         </select>
         <input
           className="px-3 py-2 rounded-md"
-          type="undefined"
+          type="number"
           placeholder="Semester"
-          {...register("semester", { required: true , maxLength: { value:1, message: "enter a number between 1 and 8"}})}
+          {...register("semester", 'test', { required: true ,  pattern:{
+            value: 2||1||3||4||5||6||7||8
+            ,
+            message: "enter a number between 1 and 8"
+           },
+           maxLength: { value:1, message: "enter a number between 1 and 8"}
+        
+
+         })}
         />
          <div className="text-red-700">{errors.semester?.message}</div>
         
@@ -111,7 +139,7 @@ const SignInScreen = ({ setSignIn }) => {
           className="px-3 py-2 rounded-md"
           type="date"
           placeholder="Date of Birth"
-          {...register("date_of_birth", { required: true })}
+          {...register("date_of_birth", { required: true , max:{value:`31-12-2006`} })}
         />
         <input
           className="px-3 py-2 rounded-md"
@@ -123,7 +151,7 @@ const SignInScreen = ({ setSignIn }) => {
           className="px-3 py-2 rounded-md"
           type="text"
           placeholder="USN"
-          {...register("USN", { required: true, pattern: {value:"1RV???????"},maxLength:{value:10, message:"enter a valid USN" }})}
+          {...register("USN", { required: true, maxLength:{value:10, message:"enter valid USN"}})}
         />
         <div className="text-red-700">{errors.USN?.message}</div>
         <input
