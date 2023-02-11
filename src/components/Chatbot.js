d5e4dcbb5c22af445aaa642bcf0b3c38e3d65717
kanupriyaanand <kanupriya.a01@gmail.com>
2023-02-11 10:02:43 +0530

    bot

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -5,15 +5,20 @@ export class Chatbot extends Component{
 
 componentDidMount(){
     
-    (function(d, m){
-        var kommunicateSettings = 
-            {"appId":"abf40351d09ed9bb152f309513163a56","popupWidget":true,"automaticChatOpenOnNavigation":true};
-        var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-        window.kommunicate = m; m._globals = kommunicateSettings;
-    })(document, window.kommunicate || {});
+    
+   
+  
+  (function(d, m){
+      var kommunicateSettings = 
+          {"appId":"abf40351d09ed9bb152f309513163a56","popupWidget":true,"automaticChatOpenOnNavigation":true};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      window.kommunicate = m; m._globals = kommunicateSettings;
+  })(document, window.kommunicate || {});
 /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+
+
 
 }
 render(){
