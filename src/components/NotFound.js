4bfc89cf81645286c56b44873de7c9104de30f2a
kanupriyaanand <kanupriya.a01@gmail.com>
2023-02-13 03:23:44 +0530

    done

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>Nahi mila, kal aana kal</div>;
+  return <div>Page not found</div>;
 };
 
 export default NotFound;
--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -10,10 +10,21 @@ import { Link } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import AddEvent from "./AddEvent";
 
 const UpcomingEvents = () => {
   let users = [];
+  const [open2, setOpen] = useState(false);
+  const [docId, setDocId] = useState("");
   const [data, setData] = useState([]);
+  const [isAdmin, setAdmin] = useState(false);
+
+  const handleOpen2 = () => setOpen(true);
+  const handleClose2 = () => setOpen(false);
+  const user = useSelector(selectUser);
+
   const getUserData = async () => {
     const querySnapshot = await getDocs(collection(db, "Upcoming_Tournaments"));
     querySnapshot.forEach((doc) => {
@@ -26,6 +37,13 @@ const UpcomingEvents = () => {
   useEffect(() => {
     getUserData();
   }, []);
+
+  const calling = (data, id) => {
+    setData(data);
+    setDocId(id);
+    data.isAdmin && setAdmin(true);
+    console.log(data.isAdmin);
+  };
 
   const handleLogout = () => {
     signOut(auth)
@@ -44,16 +62,13 @@ const UpcomingEvents = () => {
     const dataPlot = data.map((item) => {
       return [
         item.Name,
-        item.Game,
-        item.Department,
-        item.Semester,
         item.Venue,
-        item.rank,
+        item.Date,
       ];
     });
 
     autoTable(doc, {
-      head: [["Name", "Game", "Department", "Semester", "Venue", "Rank"]],
+      head: [["Competition Name", "Venue", "Date"]],
       body: dataPlot,
     });
     doc.save("table.pdf");
@@ -91,6 +106,12 @@ const UpcomingEvents = () => {
         <div className="flex w-[60vw] justify-between mt-3">
           <span>Achievements 5th sem- 2022</span>
           <span
+            onClick={handleOpen2}
+            className="ml-10 cursor-pointer bg-blue-700 text-white px-3 py-2 rounded-lg"
+          >
+            Add event
+          </span>
+          <span
             onClick={handlePrint}
             className="ml-10 cursor-pointer bg-blue-700 text-white px-3 py-2 rounded-lg"
           >
@@ -114,7 +135,15 @@ const UpcomingEvents = () => {
           </tr>
         ))}
       </table>
+      <AddEvent
+        data={data}
+        open={open2}
+        handleClose={handleClose2}
+        docId={docId}
+        admin={isAdmin}
+      />
     </Box>
+    
   );
 };
 
